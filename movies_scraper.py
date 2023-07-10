c2c9e0bf9462d322f4e821c9b38c543c4bd7937c
StupidBoi7 <126649764+StupidBoi7@users.noreply.github.com>
2023-07-10 13:42:15 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "9VmK1ijcDmX1pP9finMD4t5iEOU2"
+api_key = "b7b566f8bb9e71a4396a935e260ae9b4f01597a4"
 
 
 def search_movies(query):
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://demo.shareus.in/link-shortner-api/api?api={api_key}&url={i['href']}"
+            url = f"https://atglinks.com/api?api={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
